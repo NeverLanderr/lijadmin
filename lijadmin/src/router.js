8b1568e517cc67de32e51d6dc5a0ef02dc476318
NeverLanderr <1947638416@qq.com>
2019-10-19 14:26:57 +0800

    'xixi'

--- lijadmin/src/router.js
+++ lijadmin/src/router.js
@@ -1,17 +1,19 @@
 import React,{Component} from 'react'
+import ComponentImport from './utils/componentImport'
 import {HashRouter,Switch,Redirect,Route} from 'react-router-dom'
 import CustomNav from './component/customNav'
-import Login from './component/login'
-import Admin from './component/admin'
-import Home from './component/home'
-import User from './component/user'
+// import Login from './component/login/load'
+const Login = ComponentImport(()=>import('./component/login'))
+const Admin = ComponentImport(()=>import('./component/admin'))
+const Home = ComponentImport(()=>import('./component/home'))
+const User = ComponentImport(()=>import('./component/user'))
 
 class RootRouter extends Component{
     render(){
         return(
             <HashRouter>
             {/* 路由导航 */}
-            <CustomNav></CustomNav>
+            
             {/* 路由渲染 */}
             <Switch>
                 <Redirect exact from='/' to='/login'/>
@@ -19,6 +21,7 @@ class RootRouter extends Component{
                 <Route path='/admin' render={()=>{
                     return(
                         <Admin>
+                            <CustomNav></CustomNav>
                             <Route path='/admin/home' component={Home}/>
                             <Route path='/admin/user' component={User}/>
                         </Admin>
